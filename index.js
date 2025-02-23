e25b60f593af2ef7c0d740f7a7648d966186f345
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-23 11:03:49 -0400

    [Auth]: Add CORS support to the Express server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 
 import authRouter from "./routes/auth.js";
 import { dbConnection } from "./database/config.js";
+import cors from "cors";
 // console.log(process.env);
 
 //Crear el servidor de express
@@ -10,6 +11,10 @@ const app = express();
 
 //Base de Datos
 dbConnection();
+
+//CORS
+app.use(cors());
+
 //Directorio publico
 app.use(express.static("public"));
 
