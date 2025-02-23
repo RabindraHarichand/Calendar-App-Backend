d96b3b95329d819e4cdaee519480c6d0cb72856b
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-23 08:27:42 -0400

    [Auth]: Validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,16 +2,26 @@ import { response } from "express";
 import Usuario from "../models/Usuario.js";
 
 export const crearUsuario = async (req, res = response) => {
-  // const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const usuario = new Usuario(req.body);
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Un usuario existe con ese correo",
+      });
+    }
+
+    usuario = new Usuario(req.body);
 
     await usuario.save();
 
     res.status(201).json({
       ok: true,
-      msg: "registro",
+      uid: usuario.id,
+      name: usuario.name,
     });
   } catch (error) {
     console.log(error);
