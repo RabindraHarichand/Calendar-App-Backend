b35ac0ba54be92298426380a43c65904b23465fa
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-23 10:15:54 -0400

    [Auth]: Implement login functionality with user validation and JWT generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,15 +37,42 @@ export const crearUsuario = async (req, res = response) => {
   }
 };
 
-export const loginUsuario = (req, res = response) => {
+export const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: "login",
-    email,
-    password,
-  });
+  try {
+    const usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe con ese correo",
+      });
+    }
+
+    //Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecta",
+      });
+    }
+
+    //Generar jwt
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 export const revalidarToken = (req, res = response) => {
