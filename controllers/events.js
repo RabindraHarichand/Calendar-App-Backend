a2571f73cbb3bbb96b7af5fc2bf83a9c6c8c3826
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-23 19:20:44 -0400

    [Events]: Refactor crearEvento function to use Evento model and add error handling; update schema to enforce required fields

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,5 @@
 import { response } from "express";
-
+import Evento from "../models/Evento.js";
 export const getEventos = (req, res = response) => {
   res.status(201).json({
     ok: true,
@@ -7,13 +7,25 @@ export const getEventos = (req, res = response) => {
   });
 };
 
-export const crearEvento = (req, res = response) => {
-  //verificar que tenga el evento
+export const crearEvento = async (req, res = response) => {
+  const evento = new Evento(req.body);
 
-  res.status(201).json({
-    ok: true,
-    msg: "crearEventos",
-  });
+  try {
+    evento.user = req.uid;
+
+    const eventoGuardado = await evento.save();
+
+    res.json({
+      ok: true,
+      evento,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 export const actualizarEvento = (req, res = response) => {
